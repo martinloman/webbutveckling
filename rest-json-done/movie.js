54d5d78d1317520ea316e8e13226d1ac2557125c
martinloman <martin.loman@gmail.com>
2022-03-07 13:28:14 +0100

    Added comments.

--- rest-json-done/movie.js
+++ rest-json-done/movie.js
@@ -1,24 +1,32 @@
+const API_IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/w154"
+const API_BACKDROP_BASE_PATH = "https://image.tmdb.org/t/p/original"
+
 document.addEventListener("DOMContentLoaded", async function () {
   console.log("DOMload")
-  const title = document.getElementById("title")
+
+  // URLSearchParams kan användas för att hämta ut det id som lagts på URLen.
+  // window.location är ett objekt som innehåller information om den URL som använts.
+  console.log(window.location)
   let searchParams = new URLSearchParams(window.location.search)
+  let id = searchParams.get("id")
+  console.log(id)
 
-  console.log(searchParams.get("id"))
-
-  let movie = await getMovieFromAPI(searchParams.get("id"))
+  let movie = await getMovieFromAPI(id)
   console.log(movie)
   renderMovie(movie)
 })
 
 function renderMovie(movie) {
-  document.title = "Film: " + movie.title
+  document.title = "Movie: " + movie.title
   title.innerText = movie.title
 
+  // Sätter bakgrundsbild (från APIet) på heron
   let hero = document.getElementsByClassName("hero")[0]
   hero.style.backgroundImage = `url(${
     API_BACKDROP_BASE_PATH + movie.backdrop_path
   })`
 
+  // Samma bakgrundsbild sätts på footern
   let footer = document.getElementsByTagName("footer")[0]
   footer.style.backgroundImage = `url(${
     API_BACKDROP_BASE_PATH + movie.backdrop_path
@@ -26,21 +34,24 @@ function renderMovie(movie) {
 
   document.getElementById("synopsis").innerText = movie.overview
 
+  // En lista med genre-namn i movie.genres görs om till en sträng
   genreStrings = ""
   movie.genres.forEach(function (genre) {
     genreStrings += genre.name + " "
   })
 
   document.getElementById("genres").innerText = genreStrings
-  document.getElementById(
-    "homepage"
-  ).innerHTML = `<a href="${movie.homepage}">${movie.homepage}</a>`
+
+  //Filmens hemsida skrivs ut och länkas.
+  if (movie.homepage && movie.homepage != "") {
+    document.getElementById(
+      "homepage"
+    ).innerHTML = `<a href="${movie.homepage}">${movie.homepage}</a>`
+  }
 }
 
-const API_IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/w154"
-const API_BACKDROP_BASE_PATH = "https://image.tmdb.org/t/p/original"
-
 async function getMovieFromAPI(id) {
+  // Bygger upp URL för att hämta detaljerad info om en film, med hjälp av filmens id.
   var url = `https://api.themoviedb.org/3/movie/${id}?api_key=1a08c634ec1bc9d64558c15c3e88cdbf`
 
   console.log("Getting: " + url)
